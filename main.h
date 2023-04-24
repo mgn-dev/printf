2ad45467cbd7a35d2bfca9a43a7935c820eea2fe
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-24 14:10:08 -0700

    add template for _printf

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+/**
+ * struct format - dispatch to specified print function
+ * @identifier: type char identifier (d, i, s, c)
+ * @func: type pointer to print function
+ **/
+
+typedef struct format
+{
+	char *identifier;
+	int (*func)();
+} dispatcher;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int printf_int(va_list args);
