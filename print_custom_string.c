fe3a13c7d6b2e4293d5770637a7e14090b12199f
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-25 15:10:50 -0700

    add handler for S

---
+++ print_custom_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * printf_custom_string - print custom  string.
+ * @args: arguments.
+ * Return: the length of the string.
+ */
+
+int print_custom_string(va_list args)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + print_HEX_bis(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
